93e8be870905199f145cc94994d79a285242756e
bakiizese <bereketezeselassie@gmial.com>
2024-10-08 15:13:08 +0300

    auth updates

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-from auth.authentication import Auth
-
+from auth.authentication import Auth, _generate_uuid
+from models import storage
+from models.driver import Driver
 new = Auth()
-# new.register_user("Driver", "beki", "bereket", "zeselassie", "baki@baki", 912232, "pascode123")
-new.verify_login("Driver", "baki@baki", "pascode123")
+# new.register_user("Driver", username="bki", first_name="bereket", last_name="zeselassie", email="be@beki", phone_number=22, password_hash="passcode")
+# new.verify_login("Driver", "baki@baki", "pascode123")
+
+# user = new.create_reset_token("Driver", email="be@beki")
+# print(f"in return last check {user}")
+new.update_password("Driver", reset_token="a5e76468-2b07-49f1-80a1-2ab4fda52df2", password="hello")
+
