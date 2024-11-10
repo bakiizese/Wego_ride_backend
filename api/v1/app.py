48239d67b53071b2fb47c3294658f691807e1438
bakiizese <bereketezeselassie@gmial.com>
2024-11-10 14:57:40 +0300

    author.txt requirements.txt README.md completed apis

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,40 +6,47 @@ from flask_cors import CORS
 app = Flask(__name__)
 # CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:8001"}})
 
-swagger = Swagger(app, template_file='./swagger/main.yaml')
+swagger = Swagger(app, template_file="./swagger/main.yaml")
 
-app.register_blueprint(admin_bp, url_prefix='/api/v1/admin')
-app.register_blueprint(driver_bp, url_prefix='/api/v1/driver')
-app.register_blueprint(rider_bp, url_prefix='/api/v1/rider')
+app.register_blueprint(admin_bp, url_prefix="/api/v1/admin")
+app.register_blueprint(driver_bp, url_prefix="/api/v1/driver")
+app.register_blueprint(rider_bp, url_prefix="/api/v1/rider")
+
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({'error': 'Resource not found'}), 404
+    return jsonify({"error": "Resource not found"}), 404
+
 
 @app.errorhandler(400)
 def bad_request(error):
-    return jsonify({'error': 'Requirement missing or incorrect'}), 400
+    return jsonify({"error": "Requirement missing or incorrect"}), 400
+
 
 @app.errorhandler(405)
 def method_error(error):
-    return jsonify({'error': 'Method not allowed'}), 405
+    return jsonify({"error": "Method not allowed"}), 405
+
 
 @app.errorhandler(415)
 def unsupported(error):
-    return jsonify({'error': 'Unsupported media type'}), 415
+    return jsonify({"error": "Unsupported media type"}), 415
+
 
 @app.errorhandler(500)
 def internal_error(error):
-    return jsonify({'error': 'An internal error occurred'}), 500
+    return jsonify({"error": "An internal error occurred"}), 500
+
 
 @app.errorhandler(401)
 def unauthorized(error):
-    return jsonify({'error': 'Unauthorized to access'}), 401
+    return jsonify({"error": "Unauthorized to access"}), 401
+
 
 @app.errorhandler(403)
 def admin_resource(error):
-    return jsonify({'error': 'access not allowed'}), 403
+    return jsonify({"error": "access not allowed"}), 403
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=1)
--- rear.py
+++
@@ -1,6 +0,0 @@
-data = [('60d33c0a-8b05-4665-a5e9-6ac2455eab3e','Corolla','NULL','White',3,'301aac56-ca95-4afd-8f81-b5583cea23e2','2024-10-15 14:05:55','2024-10-15 14:05:55'),('8f0302cb-0e0e-4ad6-b2f6-839f71fccde0','nisan','NULL','Black',4,'772cdad2-3065-4474-bf7d-cc244f6a18dc','2024-10-15 14:05:07','2024-10-15 14:05:07'),('85f1c9be-aba8-4b80-8613-4925c1964eb4','Corolla','NULL','White',3,'a3d86838-1032-4176-ab5e-e8f77a38f2a3','2024-10-15 14:05:42','2024-10-15 14:05:42'),('dad730e7-d777-4e95-9774-1d205dc533c0','Vitz','NULL','Red',3,'d57c3f29-521f-4f6e-96f7-353fede215d4','2024-10-15 14:04:11','2024-10-15 14:04:11')]
-new_data = []
-for d in data:
-    new_tuple = (d[5], d[6], d[7], d[0], d[1], d[2], d[3], d[4])
-    new_data.append(new_tuple)
-print(new_data)
